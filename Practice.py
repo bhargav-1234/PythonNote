de3a77c2f421477526d7a55779f48bdd24847872
Bhargav <bhargav199805@gmail.com>
2024-08-29 17:17:51 +0530

    updated

--- Practice.py
+++ Practice.py
@@ -816,6 +816,6 @@ my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
 def my_function(**kid):
   print("His last name is " + kid["lname"])
 
-my_function(fname = "Tobias", lname = "Refsnes")
+my_function(fname = "Bhargav", lname = "Nageti")
 
 
