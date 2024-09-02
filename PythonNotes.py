d2d1ea4b1e11a190462fbde924c5103fea0ee063
Bhargav <bhargav199805@gmail.com>
2024-09-02 12:51:14 +0530

    latest notes

---
+++ PythonNotes.py
@@ -0,0 +1,16 @@
+################################################ Python Notes ####################################################
+
+######## Python Comments ####### (28-08-2024)
+""" 1.Comments can be used to explain Python code.
+    2.Comments can be used to make code more readable.
+"""
+
+######## Python Variable #######
+# Variable:
+""" 1.Variables are containers for storing the data.
+    2.Variable names must begin with a letter or an underscore, but they can be a group of both letters and digits.
+"""
+a =10
+b = "Bhargav" # this is how we declare the variable
+print(a)
+print(b)
