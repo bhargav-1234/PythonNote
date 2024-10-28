d2affd516fb22b3b14158f15a46556eaf7b33a76
Bhargav <bhargav199805@gmail.com>
2024-10-28 13:39:39 +0530

    update

--- prime.py
+++ prime.py
@@ -41,15 +41,13 @@
 #         print(num)
 
 ### ---- Most popular interview Question ---- ###
-
 def prime(num):
     if num<=1:
         return False
     for i in range(2,int(num**0.5)+1):
         if num%i==0:
-            return False
+         return False
     return True
 for num in range(2,10):
-    if prime(num):
-        print(num)
-        
+   if prime(num):
+       print(num)
