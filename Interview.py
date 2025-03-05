3eef6a3c147237d980a129add86a31689be0bfdf
Bhargav <bhargav199805@gmail.com>
2025-03-05 11:41:31 +0530

    updated

--- Interview.py
+++ Interview.py
@@ -140,32 +140,32 @@ print(spaces)
 
 #Building a pyramid in python
 
-def pyramid(n):
-        for i in range(n):
-            for j in range(i,n):
-                print(" ",end="")
-            #pdb.set_trace()
-            for j in range(i+1):
-                print('*',end="")
-            for j in range(i):
-                print('*',end="")
-            print(' ')
-pyramid(6)
+# def pyramid(n):
+#         for i in range(n):
+#             for j in range(i,n):
+#                 print(" ",end="")
+#             #pdb.set_trace()
+#             for j in range(i+1):
+#                 print('*',end="")
+#             for j in range(i):
+#                 print('*',end="")
+#             print(' ')
+# pyramid(6)
 
 #2.
-num = int(input('enter odd number:'))
-cnt=num//2
-scnt=1
-for i in range(cnt+1):
-    print(cnt*" "+"*"*scnt)
-    cnt=cnt-1
-    scnt=scnt+2
-scnt=num-2
-cnt=1
-for i in range(num//2):
-        print(cnt*" "+"*"*scnt)
-        scnt=scnt-2
-        cnt=cnt+1
+# num = int(input('enter odd number:'))
+# cnt=num//2
+# scnt=1
+# for i in range(cnt+1):
+#     print(cnt*" "+"*"*scnt)
+#     cnt=cnt-1
+#     scnt=scnt+2
+# scnt=num-2
+# cnt=1
+# for i in range(num//2):
+#         print(cnt*" "+"*"*scnt)
+#         scnt=scnt-2
+#         cnt=cnt+1
 
 #Randomizing the items of a list in python
 from random import shuffle
@@ -198,31 +198,136 @@ print(lst)
 #     print(e,end='') 
 
 # program to check if a given number is prime or not
-def prime_no(n):
-    Flag = False
-    if n < 2:
-        return Flag
-    else:
-        for i in range(2,n):
-            if n%i == 0:
-                return Flag
-            else:
-                Flag = True
-                return Flag     
-num=int(input('Enter a number: '))
-print(prime_no(num))
+# def prime_no(n):
+#     Flag = False
+#     if n < 2:
+#         return Flag
+#     else:
+#         for i in range(2,n):
+#             if n%i == 0:
+#                 return Flag
+#             else:
+#                 Flag = True
+#                 return Flag     
+# num=int(input('Enter a number: '))
+# print(prime_no(num))
 
 #2.
-def prime_no(n):
-    Flag = False
-    if n < 2:
-        return Flag
-    else:
-        for i in range(2,n):
-            if n%i == 0:
-                return Flag
-        Flag = True
-        return Flag     
-numm=int(input('Enter a number: '))
-print(prime_no(numm))
+# def prime_no(n):
+#     Flag = False
+#     if n < 2:
+#         return Flag
+#     else:
+#         for i in range(2,n):
+#             if n%i == 0:
+#                 return Flag
+#         Flag = True
+#         return Flag     
+# numm=int(input('Enter a number: '))
+# print(prime_no(numm))
     
+# implementing variable length arguments in python
+def average(*t):
+    avg=sum(t)/len(t)
+    return avg
+result1 = average(35,5,65,22,87,34,2,57)
+result2= average(5,10,15,20,25,30,35,40,45,50)
+print("Average is :",result1)
+print("Average is :",result2)
+
+#creating the instance number variables in python
+class Test:
+    def __init__(self):
+          # first instance variable inside in init function
+          self.a =5
+    def f1(self):
+        # second instance variable inside function f1
+        self.b=10
+    
+t1=Test()
+t2=Test()
+t1.f1()
+# 3rd instance variable
+t1.c=15
+print(t1.__dict__)
+print(t2.__dict__)
+
+# addition using lambda function
+f =lambda a,b :a+b
+r=f(3,7)
+print(r)
+  # OR
+print((lambda a,b :a+b)(3,7))
+
+#finding factorial using the lambda function
+# f=lambda n: 1 if n==0 else n*f(n-1)
+# print(f(5))
+
+# num= int(input("Enter a number: "))
+#   #7
+# factorial=1
+# if num < 0:
+#     print("Sorry, factorial does not exist for negative numbers")
+# elif num==0:
+#      print("The factorial of 0 is 1")
+# else:
+#     for i in range(1, num+1):
+#         factorial=factorial*i
+#     print(f"the factorial of {num} is",factorial)
+    #5040
+
+# list compression (to create a list in single line)
+   
+    # list of even numbers
+l1=[2*e for e in range(1, 10)]
+print(l1)
+
+# OR
+ # to create a list of even numbers from a given list 
+list=[23,56,65,22,62,32,65,76,33,99]
+l2=[e for e in list if e%2==0]
+print(l2)
+
+# what is the use of spilt and join function of str ?
+  #split- to convert string into list of string
+s="What is right in your mind is right in your world"
+s1=s.split(" ")
+print(s1)
+s1=s1[::-1]
+print(s1)
+  #join- to convert the list again into string
+print(" ".join(s1))
+
+# global and local variable
+
+x=5 # global variable
+def f1():
+    global x
+    x=12 # global varaible updated
+    y=10 #local variable
+    print("x=%d y=%d"%(x,y))
+
+f1()
+print(x)
+
+# type of conversion basics
+x =int('123')
+y=float('123.456')
+a=complex(1+3j)
+# b=str(12)
+c=bool('True')
+d=bin(25) #binary
+e=oct(25)
+f=hex(25)
+g=ord('A') #char to unicode
+h=chr(98) #unicode to character
+print(x,y,a,c,d,e,f,g,h,sep="\n")
+
+# type conversion
+
+x=6
+print(type(x))
+s='123'
+print(type(s))
+# print(str(x)+s)
+print(x+int(s)) 
